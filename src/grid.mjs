8bdfd5f7007df7813ed8c033ed554a5497d66e4e
frensing <dev@frensing.de>
2024-04-27 10:43:25 +0300

    initial grid

--- src/grid.mjs
+++ src/grid.mjs
@@ -1,3 +1,11 @@
 export class Grid {
-  
+  grid
+
+  constructor(width, height) {
+    this.grid = Array.from(Array(height), () => Array(width).fill(false))
+  }
+
+  toString() {
+    return this.grid.map(row => row.map(x => x ? 'X' : '.').join('')).join('\n') + '\n'
+  }
 }
--- test/grid.test.mjs
+++ test/grid.test.mjs
@@ -1,9 +1,19 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import Grid from "../src/grid.mjs";
+import { Grid } from "../src/grid.mjs";
 
-describe("Example test fixture", () => {
-  test("Example test", () => {
-    expect(true).to.be.true
+describe("Grid", () => {
+  test("initial grid has empty cells", () => {
+    const grid = new Grid(3, 3)
+    
+    expect(grid.toString()).to.equal(normalize(
+      `...
+       ...
+       ...`
+    ))
   });
 });
+
+function normalize(s) {
+  return s.replaceAll(' ', '').trim() + '\n'
+}
