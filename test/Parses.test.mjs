3179cb04c8985778906dd7a0840f3e3db55cfa23
frensing <dev@frensing.de>
2024-04-27 11:10:33 +0300

    parse long patterns

--- test/Parses.test.mjs
+++ test/Parses.test.mjs
@@ -6,6 +6,11 @@ const GLIDER =
   `x = 3, y = 3
    bo$2bo$3o!`
 
+const GOSPER_GLIDER_GUN = 
+  `x = 36, y = 9, rule = B3/S23
+   24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4b
+   obo$10bo5bo7bo$11bo3bo$12b2o!`
+
 describe('Parser', () => {
   test('can extract width and height from rle', () => {
     const parser = new Parser(GLIDER)
@@ -16,5 +21,15 @@ describe('Parser', () => {
   test('can extract the pattren from rle', () => {
     const parser = new Parser(GLIDER)
     expect(parser.getPattern()).to.equal('bo$2bo$3o!')
+  })
+
+  test('can extract long patterns', () => {
+    const parser = new Parser(GOSPER_GLIDER_GUN)
+
+    expect(parser.getWidth()).to.equal(36)
+    expect(parser.getHeight()).to.equal(9)
+    expect(parser.getPattern()).to.equal(
+      '24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4bobo$10bo5bo7bo$11bo3bo$12b2o!'
+    )
   })
 })
