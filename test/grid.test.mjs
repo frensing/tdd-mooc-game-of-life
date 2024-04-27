6bb699e4b748b8b824eb98d3fa529c1c5daf619e
frensing <dev@frensing.de>
2024-04-27 10:29:47 +0300

    init

---
+++ test/grid.test.mjs
@@ -0,0 +1,9 @@
+import { describe, test } from "vitest";
+import { expect } from "chai";
+import Grid from "../src/grid.mjs";
+
+describe("Example test fixture", () => {
+  test("Example test", () => {
+    expect(true).to.be.true
+  });
+});
